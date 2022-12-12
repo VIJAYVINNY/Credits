42defdc8ff258becac6130126379adbe1c39ad13
Vijay Kumar <vijay1021.gummadi@gmail.com>
2022-12-12 05:02:52 -0500

    Component is created

---
+++ src/app/shann-ji/shann-ji.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shann-ji',
+  templateUrl: './shann-ji.component.html',
+  styleUrls: ['./shann-ji.component.scss']
+})
+export class ShannJiComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
