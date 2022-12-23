10fe25653b52a09bf8743aa68b0144ab105925cf
Vijay Kumar <vijay1021.gummadi@gmail.com>
2022-12-23 09:29:00 -0500

    ooo

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -10,26 +10,49 @@ import { ReactiveFormsModule } from '@angular/forms';
   styleUrls: ['./todo.component.scss']
 })
 export class TodoComponent implements OnInit {
-
+  constructor(){}
 
   registrationForm=new FormGroup({
-    userName:new FormControl('Sanjay'),
+    userName:new FormControl(''),
     password:new FormControl(''),
     confirmPassword:new FormControl(''),
-    
+
     address:new FormGroup({
       city:new FormControl(''),
       state:new FormControl(''),
       postalNo:new FormControl(''),
     })
   });
+
 address: any;
-
-
-  constructor(){}
-
-  contactList:any;
+contactList:any;
 
   ngOnInit(): void { }
+// //With help of patchvalue we will get required data(Optional)
+  // loadApiData(){
+  //   this.registrationForm.patchValue({
+  //     userName:"vijay",
+  //     password:"Vijay1021@",
+  //     confirmPassword:"Vijay1021@",
+  //     // address:{
+  //     //   city:"Kalluru",
+  //     //   state:"Telangana",
+  //     //   postalNo:507209
+  //     // }
+  //   })
+  //     }
 
+  //// 
+      loadApiData(){
+        this.registrationForm.setValue({
+          userName:"vijay",
+          password:"Vijay1021@",
+          confirmPassword:"Vijay1021@",
+          address:{
+            city:"Kalluru",
+            state:"Telangana",
+            postalNo:507209
+          }
+        })
+          }
 }
