4660cb333bcc7200e3913ecb286960af47c0f9e3
Vijay Kumar <vijay1021.gummadi@gmail.com>
2023-01-10 09:33:13 -0500

    data Added

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialComponent } from './material.component';
-
-
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatIconModule} from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
+import {MatDividerModule} from '@angular/material/divider';
 
 @NgModule({
   declarations: [MaterialComponent],
   imports: [
     CommonModule,
+    MatCheckboxModule,
+    MatIconModule,
+    MatCardModule,MatDividerModule
 
   ]
 })
