077b79bb3d391831378909b2235435c5c4bba191
Vijay Kumar <vijay1021.gummadi@gmail.com>
2023-01-11 09:35:55 -0500

    material UI is working

--- src/app/materials/materials.component.ts
+++ src/app/materials/materials.component.ts
@@ -1,13 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-import { MaterialsModule } from './materials.module';
 @Component({
   selector: 'app-materials',
   templateUrl: './materials.component.html',
   styleUrls: ['./materials.component.scss']
 })
 export class MaterialsComponent implements OnInit {
-
+notifivation=6;
   constructor() { }
+  hidden = false;
+
+
+  toggleBadgeVisibility() {
+    this.hidden = !this.hidden;
+  }
 
   ngOnInit(): void {
   }
--- src/app/materials/materials.module.ts
+++ src/app/materials/materials.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialsComponent } from './materials.component';
 import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatIconModule} from '@angular/material/icon';
+import {MatBadgeModule} from '@angular/material/badge';
 
 
 @NgModule({
   declarations: [MaterialsComponent],
+
   imports: [
     CommonModule,
-    MatButtonToggleModule
+    MatButtonToggleModule,
+    MatIconModule,MatBadgeModule
   ],
   exports:[]
 })
