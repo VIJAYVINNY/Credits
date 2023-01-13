e5656be74707a30591ae2548836abd58cab7a6d3
Vijay Kumar <vijay1021.gummadi@gmail.com>
2023-01-13 00:37:56 -0500

    sidebar working

--- src/app/materials/materials.module.ts
+++ src/app/materials/materials.module.ts
@@ -4,7 +4,14 @@ import { MaterialsComponent } from './materials.component';
 import {MatButtonToggleModule} from '@angular/material/button-toggle';
 import {MatIconModule} from '@angular/material/icon';
 import {MatBadgeModule} from '@angular/material/badge';
-
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatCardModule} from '@angular/material/card';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatSliderModule} from '@angular/material/slider';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 @NgModule({
   declarations: [MaterialsComponent],
@@ -12,7 +19,10 @@ import {MatBadgeModule} from '@angular/material/badge';
   imports: [
     CommonModule,
     MatButtonToggleModule,
-    MatIconModule,MatBadgeModule
+    MatIconModule,MatBadgeModule,
+    MatProgressSpinnerModule,
+    MatCardModule,MatRadioModule,MatSliderModule,MatToolbarModule,MatTooltipModule,MatTabsModule
+    ,MatSidenavModule
   ],
   exports:[]
 })
