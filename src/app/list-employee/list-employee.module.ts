6309a9eed802d86f4c2ff7b5c9984a1a86b1bc3f
Vijay Kumar <vijay1021.gummadi@gmail.com>
2023-01-19 09:31:30 -0500

    data added

--- src/app/list-employee/list-employee.module.ts
+++ src/app/list-employee/list-employee.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListEmployeeComponent } from './list-employee.component';
 import {MatExpansionModule} from '@angular/material/expansion';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
 
 
 @NgModule({
   declarations: [ ListEmployeeComponent],
   imports: [
-    CommonModule,MatExpansionModule
+    CommonModule,MatExpansionModule,
+    MatCardModule,MatButtonModule
   ]
 })
 export class ListEmployeeModule { }
