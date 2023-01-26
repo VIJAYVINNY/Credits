44aabaaa52d061bf790078f32611f58f2267427e
Vijay Kumar <vijay1021.gummadi@gmail.com>
2023-01-26 07:35:09 -0500

    UICRUD

--- src/app/dialog/dialog.component.ts
+++ src/app/dialog/dialog.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Food } from './dialog.model';
+import { Vehicles } from './dialog.model';
 
 @Component({
   selector: 'app-dialog',
@@ -12,11 +12,18 @@ export class DialogComponent implements OnInit {
 
   ngOnInit(): void { }
 
-  foods: Food[] = [
-    {value: 'steak-0', viewValue: 'Steak'},
-    {value: 'pizza-1', viewValue: 'Pizza'},
-    {value: 'tacos-2', viewValue: 'Tacos'},
+  vehicles: Vehicles[] = [
+    {value: 'TATA', viewValue: 'TATA'},
+    {value: 'MARUTI', viewValue: 'MARUTI'},
+    {value: 'VOLKSWAGNOR', viewValue: 'WAGNOR'},
   ];
+  myFilter = (d: Date | null): boolean => {
+    const day = (d || new Date()).getDay();
+    // Prevent Saturday and Sunday from being selected.
+    return day !== 0 && day !== 6;
+  };
+
+  quality=["Below","Average","Medium","Good"];
 
   }
 
--- src/app/dialog/dialog.model.ts
+++ src/app/dialog/dialog.model.ts
@@ -1,4 +1,4 @@
- export interface Food {
+ export interface Vehicles {
   value: string;
   viewValue: string;
 }
