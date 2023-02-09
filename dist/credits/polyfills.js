4a1afba2ecdfcf66cc5ad11358e6f69a41efdcfd
Vijay Kumar <vijay1021.gummadi@gmail.com>
2023-02-09 14:30:42 +0530

    Successfully created new branch and running code successfully

--- dist/credits/polyfills.js
+++ dist/credits/polyfills.js
@@ -7,7 +7,7 @@
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Users\kishore\Documents\GitHub\credits\src\polyfills.ts */"hN/g");
+module.exports = __webpack_require__(/*! D:\Credits\src\polyfills.ts */"hN/g");
 
 
 /***/ }),
