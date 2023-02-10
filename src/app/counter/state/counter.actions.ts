aca695f718b0e4ee34461ff3a59102b6f35d39cb
Vijay Kumar <vijay1021.gummadi@gmail.com>
2023-02-10 20:07:56 +0530

    Change Detection Strategy

--- src/app/counter/state/counter.actions.ts
+++ src/app/counter/state/counter.actions.ts
@@ -1,8 +1,8 @@
 import { createAction, props } from "@ngrx/store";
 
-export const increment=createAction('increment');
-export const decrement=createAction('decrement');
-export const reset=createAction('reset');
+export const increment=createAction('[CounterComponent] increment');
+export const decrement=createAction('[CounterComponent] decrement');
+export const reset=createAction('[CounterComponent] reset');
 
 export const customIncrement=createAction('customincrement',
 props<{value:number}>()
--- src/app/counter/state/counter.state.ts
+++ src/app/counter/state/counter.state.ts
@@ -2,3 +2,6 @@ export const initialState=
 {
   counter:0
 }
+export interface CounterState{
+  counter:4;
+}
